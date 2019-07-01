ae36baedc4424c32653f88caea0d7be6cd34ac2a
OrangeManDavid <OrangeHeroDavid@163.com>
2019-07-01 22:07:00 +0800

    GoLearning cf main.go

---
+++ 2.程序结构/cf/main.go
@@ -0,0 +1,43 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 43.
+//!+
+
+// Cf converts its numeric argument to Celsius and Fahrenheit.
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"gopl.io/ch2/tempconv"
+)
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		t, err := strconv.ParseFloat(arg, 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+			os.Exit(1)
+		}
+		f := tempconv.Fahrenheit(t)
+		c := tempconv.Celsius(t)
+		fmt.Printf("%s = %s, %s = %s\n",
+			f, tempconv.FToC(f), c, tempconv.CToF(c))
+	}
+}
+
+//!-
+
+// Testing command
+
+// PS C:\Users\OrangeMan\go\myGitHub\gopl.io\ch2\cf> go build
+// PS C:\Users\OrangeMan\go\myGitHub\gopl.io\ch2\cf> ./cf 32
+// 32°F = 0°C, 32°C = 89.6°F
+// PS C:\Users\OrangeMan\go\myGitHub\gopl.io\ch2\cf> ./cf 212
+// 212°F = 100°C, 212°C = 413.6°F
+// PS C:\Users\OrangeMan\go\myGitHub\gopl.io\ch2\cf> ./cf -40
+// -40°F = -40°C, -40°C = -40°F
+// PS C:\Users\OrangeMan\go\myGitHub\gopl.io\ch2\cf>
